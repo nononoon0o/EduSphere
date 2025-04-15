d5640bca528de373a5ac8728b1f6bf8943165b7a
Gishin <167405120+Gishinbazu@users.noreply.github.com>
2025-04-15 22:59:53 +0900

    Delete Low-code1 directory

--- Low-code1/store/index.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useGameStore = create((set) => ({
-  inventory: [],
-  addElement: (element) => set((state) => ({ inventory: [...state.inventory, element] })),
-}));
