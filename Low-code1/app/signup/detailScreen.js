30115ae5823a6a76ebba1df436ac52b46861ad7c
nonooono <joo010410@gmail.com>
2025-04-23 21:14:30 +0900

    오류 해결

--- Low-code1/app/signup/detailScreen.js
+++ Low-code1/app/signup/detailScreen.js
@@ -3,7 +3,7 @@ import { View, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-nat
 import { useRouter } from 'expo-router';
 import Icon from "react-native-vector-icons/FontAwesome";
 import axios from 'axios';
-import styles from '../../style/signupStyle/detailStyle';
+import styles from '../../style/signupStyle/DetailStyle';
 
 const DetailForm = () => {
   const router = useRouter();
