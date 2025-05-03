c4e0260ca7564bd5dca7947ee4ef726f8ed251c4
Gishin <167405120+Gishinbazu@users.noreply.github.com>
2025-05-03 15:20:24 +0900

    Delete EduSphere directory

--- EduSphere/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// 👇 .glb 확장자를 에셋으로 추가
-config.resolver.assetExts.push('glb');
-
-module.exports = config;
