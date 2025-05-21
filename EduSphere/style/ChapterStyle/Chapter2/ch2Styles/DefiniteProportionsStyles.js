f575c915e641af477fce69c44cd975705dc8e95b
Gishin <167405120+Gishinbazu@users.noreply.github.com>
2025-05-22 03:17:48 +0900

    Delete EduSphere/style directory

--- EduSphere/style/ChapterStyle/Chapter2/ch2Styles/DefiniteProportionsStyles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: '#fff', padding: 20 },
-    title: { fontSize: 18, fontWeight: 'bold', marginBottom: 15 },
-    text: { fontSize: 16 },
-  });
-
-export default styles;
