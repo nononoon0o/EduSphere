535d20faa9faea27d85f62ff4fc170361e95de73
Gishin <167405120+Gishinbazu@users.noreply.github.com>
2025-05-27 02:53:29 +0900

    Update index.js
    Update index

--- EduSphere/app/chapters/Chapter2/index.js
+++ EduSphere/app/chapters/Chapter2/index.js
@@ -11,7 +11,7 @@ import {
 import { useRouter } from 'expo-router';
 import { Feather, FontAwesome5 } from '@expo/vector-icons';
 import { useTranslation } from 'react-i18next';
-import styles from '../../../style/ChapterStyle/Chapter2/indexStyles';
+import styles from '../../../style/sharedIndexStyles';
 
 export default function Chapter2Index() {
   const router = useRouter();
