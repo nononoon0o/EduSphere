6e4a539d0e2ddca05c0bbdb4204d818f38356e9c
Gishin <167405120+Gishinbazu@users.noreply.github.com>
2025-06-03 15:59:13 +0900

    Update assignmentDetailStyle.js
    Update assignmentDetailStyle

--- EduSphere/style/assignments/assignmentDetailStyle.js
+++ EduSphere/style/assignments/assignmentDetailStyle.js
@@ -104,11 +104,6 @@ const assignmentDetailStyle = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 16,
     alignSelf: 'flex-start',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 4,
-    elevation: 4,
   },
 });
 
