bfc790459a92c4a37a9522a8bce4b4bee1c4b51a
Gishin <167405120+Gishinbazu@users.noreply.github.com>
2025-06-09 14:11:01 +0900

    Update screenMap.js
    Update screenMap.js

--- EduSphere/app/routes/screenMap.js
+++ EduSphere/app/routes/screenMap.js
@@ -1,60 +1,60 @@
 const screenMap = {
-  // CHAPTER TITLES ➝ Overview Screens
+  // 🌐 CHAPITRE PRINCIPAL – Accès global
   "chapterTitles.Chapter1": "/chapters/Chapter1",
   "chapterTitles.Chapter2": "/chapters/Chapter2",
   "chapterTitles.Chapter3": "/chapters/Chapter3",
   "chapterTitles.Chapter4": "/chapters/Chapter4",
 
-  // CHAPTER 1
+  // 📘 CHAPITRE 1
   "chapter1.subtitles.01": "/chapters/Chapter1/Chapter1_01",
-  "chapter1_01.sections.concept": "/chapters/Chapter1/chp1/chp1_01/learnPages/ConceptSummaryScreen",
-  "chapter1_01.sections.learn": "/chapters/Chapter1/chp1/chp1_01/learnPages/LearnScreen",
-  "chapter1_01.sections.video": "/chapters/Chapter1/chp1/chp1_01/learnPages/VideoLearningScreen",
-
-  "chapter1_02.sections.concept": "/chapters/Chapter1/chp1/chp1_02/ConceptSummaryScreen",
-  "chapter1_02.sections.glb": "/chapters/Chapter1/chp1/chp1_02/GLBViewer",
-  "chapter1_02.sections.video": "/chapters/Chapter1/chp1/chp1_02/VideoLearningScreen",
-
-  "chapter1_03.cards.item3": "/chapters/Chapter1/chp1/chp1_03/ActivationEnergyScreen",
-  "chapter1_03.cards.item6": "/chapters/Chapter1/chp1/chp1_03/CatalystScreen",
-  "chapter1_03.cards.item2": "/chapters/Chapter1/chp1/chp1_03/EnergyReleaseScreen",
-  "chapter1_03.cards.item5": "/chapters/Chapter1/chp1/chp1_03/EnergyTransferRoleScreen",
-  "chapter1_03.cards.item1": "/chapters/Chapter1/chp1/chp1_03/GasReactionRatioScreen",
-  "chapter1_03.cards.item7": "/chapters/Chapter1/chp1/chp1_03/PracticalApplicationScreen",
+  "chapter1_01.concept": "/chapters/Chapter1/chp1/chp1_01/ConceptSummaryScreen",
+  "chapter1_01.learn": "/chapters/Chapter1/chp1/chp1_01/LearnScreen",
+  "chapter1_01.video": "/chapters/Chapter1/chp1/chp1_01/VideoLearningScreen",
+  "chapter1_01.eval": "/chapters/Chapter1/chp1/chp1_01/EvaluationScreen",
 
   "chapter1.subtitles.02": "/chapters/Chapter1/Chapter1_02",
-  "chapter1.subtitles.03": "/chapters/Chapter1/Chapter1_03",
+  "chapter1_02.concept": "/chapters/Chapter1/chp1/chp1_02/ConceptSummaryScreen",
+  "chapter1_02.learn": "/chapters/Chapter1/chp1/chp1_02/LearnScreen",
+  "chapter1_02.video": "/chapters/Chapter1/chp1/chp1_02/VideoLearningScreen",
+  "chapter1_02.eval": "/chapters/Chapter1/chp1/chp1_02/EvaluationScreen",
 
-  // CHAPTER 2
+  "chapter1.subtitles.03": "/chapters/Chapter1/Chapter1_03",
+  "chapter1_03.concept": "/chapters/Chapter1/chp1/chp1_03/ConceptSummaryScreen",
+  "chapter1_03.learn": "/chapters/Chapter1/chp1/chp1_03/LearnScreen",
+  "chapter1_03.video": "/chapters/Chapter1/chp1/chp1_03/VideoLearningScreen",
+  "chapter1_03.eval": "/chapters/Chapter1/chp1/chp1_03/EvaluationScreen",
+
+  // 📗 CHAPITRE 2
   "chapter2.subtitles.01": "/chapters/Chapter2/Chapter2_01",
-  "chapter2_01.menu.item1": "/chapters/Chapter2/chp2/chp2_01/AtmosphereComposition",
-  "chapter2_01.menu.item6": "/chapters/Chapter2/chp2/chp2_01/ClimateChangeEffects",
-  "chapter2_01.menu.item3": "/chapters/Chapter2/chp2/chp2_01/ClimateChemistry",
-  "chapter2_01.menu.item4": "/chapters/Chapter2/chp2/chp2_01/EnergyTransferMechanisms",
-  "chapter2_01.menu.item2": "/chapters/Chapter2/chp2/chp2_01/GreenhouseEffect",
-  "chapter2_01.menu.item5": "/chapters/Chapter2/chp2/chp2_01/HumanImpactClimate",
+  "chapter2_01.item1": "/chapters/Chapter2/chp2/chp2_01/AtmosphereComposition",
+  "chapter2_01.item2": "/chapters/Chapter2/chp2/chp2_01/ClimateChangeEffects",
+  "chapter2_01.item3": "/chapters/Chapter2/chp2/chp2_01/ClimateChemistry",
+  "chapter2_01.item4": "/chapters/Chapter2/chp2/chp2_01/EnergyTransferMechanisms",
+  "chapter2_01.item5": "/chapters/Chapter2/chp2/chp2_01/GreenhouseEffect",
+  "chapter2_01.item6": "/chapters/Chapter2/chp2/chp2_01/HumanImpactClimate",
 
   "chapter2.subtitles.02": "/chapters/Chapter2/Chapter2_02",
-  "chapter2_02.menu.item1": "/chapters/Chapter2/chp2/chp2_02/ConservationOfMass",
-  "chapter2_02.menu.item2": "/chapters/Chapter2/chp2/chp2_02/DefiniteProportions",
+  "chapter2_02.item1": "/chapters/Chapter2/chp2/chp2_02/ConservationOfMass",
+  "chapter2_02.item2": "/chapters/Chapter2/chp2/chp2_02/DefiniteProportions",
 
   "chapter2.subtitles.03": "/chapters/Chapter2/Chapter2_03",
-  "chapter2_03.menu.item3": "/chapters/Chapter2/chp2/chp2_03/GasEnergyExchange",
-  "chapter2_03.menu.item1": "/chapters/Chapter2/chp2/chp2_03/PressureDetail",
-  "chapter2_03.menu.item2": "/chapters/Chapter2/chp2/chp2_03/WindDetail",
+  "chapter2_03.item1": "/chapters/Chapter2/chp2/chp2_03/GasEnergyExchange",
+  "chapter2_03.item2": "/chapters/Chapter2/chp2/chp2_03/PressureDetail",
+  "chapter2_03.item3": "/chapters/Chapter2/chp2/chp2_03/WindDetail",
 
   "chapter2.subtitles.04": "/chapters/Chapter2/Chapter2_04",
-  "chapter2_04.menu.item1": "/chapters/Chapter2/chp2/chp2_04/ForecastingModeling",
-  "chapter2_04.menu.item2": "/chapters/Chapter2/chp2/chp2_04/ScientificPrinciples",
-  "chapter2_04.menu.item3": "/chapters/Chapter2/chp2/chp2_04/WeatherImpact",
+  "chapter2_04.item1": "/chapters/Chapter2/chp2/chp2_04/ForecastingModeling",
+  "chapter2_04.item2": "/chapters/Chapter2/chp2/chp2_04/ScientificPrinciples",
+  "chapter2_04.item3": "/chapters/Chapter2/chp2/chp2_04/WeatherImpact",
 
-  // CHAPTER 3
+  // 📙 CHAPITRE 3
   "chapter3.subtitles.01": "/chapters/Chapter3/Chapter3_01",
   "chapter3_01.screen": "/chapters/Chapter3/chp3/chp3_01/ChangesInMatterScreen",
+
   "chapter3.subtitles.02": "/chapters/Chapter3/Chapter3_02",
   "chapter3_02.screen": "/chapters/Chapter3/chp3/chp3_02/ChemicalEquationsScreen",
 
-  // CHAPTER 4
+  // 📒 CHAPITRE 4
   "chapter4.subtitles.01": "/chapters/Chapter4/Chapter4_01",
   "chapter4.subtitles.02": "/chapters/Chapter4/Chapter4_02",
 };
